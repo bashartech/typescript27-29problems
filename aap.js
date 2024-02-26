fb29ef304e4877ebc147dc0c87a64a03a43e21e6
bashartech <144168218+bashartech@users.noreply.github.com>
2024-02-26 19:38:53 +0500

    Add files via upload

---
+++ aap.js
@@ -0,0 +1,49 @@
+// 27-29 problems
+// 27 project 
+var alien_color3 = ["green", "yellow", "red"];
+if (alien_color3 = [alien_color3[0]]) {
+    console.log("The player just earned 5 points for shooting the alien");
+}
+if (alien_color3 = [alien_color3[1]]) {
+    console.log("The player just earned 10 points for shooting the alien");
+}
+if (alien_color3 = [alien_color3[2]]) {
+    console.log("The player just earned 15 points for shooting the alien");
+}
+// 28 project
+var age = 56;
+if (age < 2) {
+    console.log("Person is baby");
+}
+if (age > 1 && age < 4) {
+    console.log("Person is toodler");
+}
+if (age > 3 && age < 13) {
+    console.log("Person is a kid");
+}
+if (age > 12 && age < 20) {
+    console.log("Person is teenager");
+}
+if (age > 19 && age < 65) {
+    console.log("Person is an adult");
+}
+if (age > 64 && age > 65) {
+    console.log("Person is an elder");
+}
+// 29 project
+var favorite_fruit = ["Banana", "Grapes", "Apple"];
+if (favorite_fruit[0] == favorite_fruit[9]) {
+    console.log("You really like banana");
+}
+if (favorite_fruit[0] == favorite_fruit[2]) {
+    console.log("You really like banana");
+}
+if (favorite_fruit[0] == favorite_fruit[1]) {
+    console.log("You really like banana");
+}
+if (favorite_fruit[0] == favorite_fruit[0]) {
+    console.log("You really like banana");
+}
+if (favorite_fruit[0] == favorite_fruit[3]) {
+    console.log("You really like banana");
+}
